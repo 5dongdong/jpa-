ac5c7ec53a93ffc6955ca70fba9a9571c7225e99
오동현 <dhgus44@naver.com>
2023-11-14 23:59:07 +0900

    feat: v2

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -17,11 +17,25 @@ public class MemberApiController {
   private final MemberService memberService;
 
   @PostMapping("/api/v1/members")
-  public CreateMemberResponse saveMembev1(@RequestBody @Valid Member member) {
+  public CreateMemberResponse saveMember1(@RequestBody @Valid Member member) {
     Long id = memberService.join(member);
     return new CreateMemberResponse(id);
   }
 
+  @PostMapping("/api/v2/members")
+  public CreateMemberResponse saveMember2(@RequestBody @Valid CreateMemberRequest request) {
+
+    Member member = new Member();
+    member.setName(request.getName());
+
+    Long id  = memberService.join(member);
+    return new CreateMemberResponse(id);
+  }
+
+  @Data
+  static class CreateMemberRequest {
+    private String name;
+  }
   @Data
   static class  CreateMemberResponse {
     private Long id;
